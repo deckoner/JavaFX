efa19fe75c330bfa901ac44228513b567bcec711
Deckoner <briankira100@gmail.com>
2022-12-01 13:24:33 +0100

    hg

---
+++ Hilos/TcpUdp/Ejemplo9/Persona.java
@@ -0,0 +1,31 @@
+package Ejemplo9;
+
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+
+	String nombre;
+	int edad;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+}
